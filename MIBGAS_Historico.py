56bd45ee496c5eb0aa189ab7aa574f9340b8eef6
javiglejun <javiglejun@gmail.com>
2026-09-18 09:23:58 +0200

    Update MIBGAS_Historico.py

--- MIBGAS_Historico.py
+++ MIBGAS_Historico.py
@@ -43,7 +43,7 @@ COLUMNAS_SELECCIONADAS = [
     "Maximum Price\n[EUR/MWh]",
     "Minimum Price\n[EUR/MWh]",
     "Price difference between purchases and sales\n[%]",
-    "Auction Volume Traded®[MWh]",
+    "Auction Volume Traded\n[MWh]",
     "OTC Volume Registered [MWh]",
     "Volume Traded\n[MWh]",
 ]
@@ -65,7 +65,7 @@ COLUMNAS_NUMERICAS = [
     "Maximum Price\n[EUR/MWh]",
     "Minimum Price\n[EUR/MWh]",
     "Price difference between purchases and sales\n[%]",
-    "Auction Volume Traded®[MWh]",
+    "Auction Volume Traded\n[MWh]",
     "OTC Volume Registered [MWh]",
     "Volume Traded\n[MWh]",
 ]
